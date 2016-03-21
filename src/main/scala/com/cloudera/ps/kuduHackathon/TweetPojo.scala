ac77889ec7abdc9bc78603b569b5a5a5a21149a8
keebler <keebler@cloudera.com>
2016-03-21 14:04:32 -0400

    initial pojo commit

---
+++ src/main/scala/com/cloudera/ps/kuduHackathon/TweetPojo.scala
@@ -0,0 +1,8 @@
+package com.cloudera.ps.kuduHackathon
+
+/**
+  * Created by keebler on 3/21/16.
+  */
+class TweetPojo(val screenName:String, val text:String, val retweets:Int, val favorites:Int) {
+
+}
